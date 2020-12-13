fd3408e6f6c69566fc7a886700f6100098ebc65c
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-12-13 22:13:49 +0000

    heroes component

--- first-app/src/app/heroes/heroes.component.ts
+++ first-app/src/app/heroes/heroes.component.ts
@@ -15,12 +15,17 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() {
     
    }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
